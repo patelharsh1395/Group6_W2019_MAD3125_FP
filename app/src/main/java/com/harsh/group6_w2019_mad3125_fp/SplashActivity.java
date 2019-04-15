fb28f8746e29e272a9ac5e6a87f95c2a0edc8063
Sushmitha <sushmitha51094@gmail.com>
2019-04-15 18:55:30 -0400

    added changes

--- app/src/main/java/com/harsh/group6_w2019_mad3125_fp/SplashActivity.java
+++ app/src/main/java/com/harsh/group6_w2019_mad3125_fp/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
