d56758ba815b0c3febde9069e06d3bf7f32d6a7f
harsh1395 <patelharsh1395@gmail.com>
2019-04-16 20:50:13 -0400

    web view working

--- app/src/main/java/com/harsh/group6_w2019_mad3125_fp/AboutUsFragment.java
+++ app/src/main/java/com/harsh/group6_w2019_mad3125_fp/AboutUsFragment.java
@@ -7,11 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class AboutUsFragment extends Fragment {
 
-
+    private WebView webView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -22,8 +24,19 @@ public class AboutUsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-         TextView aboutus_message =  view.findViewById(R.id.text_aboutus_message);
-         aboutus_message.setText("We help our customers to Customize journal entries, accrual tracking capabilities, and automatic tax filing make payroll easy. From each pay period to year-end, we’ve got your payroll processing covered.");
+
+        this.webView = view.findViewById(R.id.myWebView);
+        this.webView.setWebViewClient(new WebViewClient());
+        this.webView.loadUrl("https://www.google.com");
+
+
+
+
+//         TextView aboutus_message =  view.findViewById(R.id.text_aboutus_message);
+//         aboutus_message.setText("We help our customers to Customize journal entries, " +
+//                               "\naccrual tracking capabilities, and automatic tax filing " +
+//                                "\nmake payroll easy. From each pay period to year-end, we’ve " +
+//                                "\ngot your payroll processing covered.");
     }
 
 
